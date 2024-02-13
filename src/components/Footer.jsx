d32c7a26a2cb4c26523c9c155fb985326054ed1a
Devfrank94 <francescomurro94@gmail.com>
2024-02-13 21:17:06 +0100

    add wave style in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,13 @@ function Footer() {
 
       return  <footer>
                 <p> Created by DevFrank94 Copyright &copy; {yearUpdate} </p>
+                {/* Wave animations */}
+                <div class="waves">
+                <div class="wave" id="wave1"></div>
+                <div class="wave" id="wave2"></div>
+                <div class="wave" id="wave3"></div>
+                <div class="wave" id="wave4"></div>
+                </div>
               </footer>
 }
 
