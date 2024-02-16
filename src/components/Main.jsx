d34e5218fb700b61ff71748e977ab512069de87e
Devfrank94 <francescomurro94@gmail.com>
2024-02-16 12:09:48 +0100

    add props and map component note

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Note from "./Note";
+import notes from "../notes";
+
+console.log(notes);
+
+function createNote(note){
+  return (<Note 
+  key={note.id}
+  title={note.title}
+  content={note.content}
+  />
+
+  )
+}
 
 function Main() {
     return (
       <main>
-        <Note/>
+        {notes.map((createNote))}
       </main>
       
 
